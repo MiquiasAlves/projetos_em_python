2ec75b9b52dd0a60108d9d371f049ea112d8bf05
Miquias Alves <miiquiasallves2015@gmail.com>
2023-11-05 14:52:11 -0300

    Update precificador_de_roupa.py
    Comentários ao código

--- precificador_de_roupa.py
+++ precificador_de_roupa.py
@@ -16,34 +16,34 @@ while True:
     print('---------' * 5)
 
     
-    if resposta == 1:
+    if resposta == 1: # Verifica a escolha do usuário
         print('---------' * 5)
         print('Você escolheu a opção camiseta.')
         print('---------' * 5)
-        valor_de_compra_camiseta = float(input('Qual o valor de compra do produto: R$ '))
-        valor_de_custo_camiseta = valor_de_compra_camiseta + (valor_de_compra_camiseta * 0.20)
-        valor_com_lucro_camiseta = valor_de_custo_camiseta + (valor_de_custo_camiseta * 0.75)
-        print('O valor de venda deste produto é R$ {:.2f}'.format(valor_com_lucro_camiseta))
-    elif resposta == 2:
+        valor_de_compra_camiseta = float(input('Qual o valor de compra do produto: R$ ')) # Solicita o valor de compra da camiseta
+        valor_de_custo_camiseta = valor_de_compra_camiseta + (valor_de_compra_camiseta * 0.20)  # Calcula o custo com uma margem de 20%
+        valor_com_lucro_camiseta = valor_de_custo_camiseta + (valor_de_custo_camiseta * 0.75)  # Calcula o lucro com uma margem de 75%
+        print('O valor de venda deste produto é R$ {:.2f}'.format(valor_com_lucro_camiseta)) # Exibe o valor de venda
+    elif resposta == 2: # Verifica a escolha do usuário
         print('---------' * 5)
         print('Você escolheu a opção Calça.')
         print('---------' * 5)
-        valor_de_compra_calça = float(input('Qual o valor de compra do produto: R$ '))
-        valor_de_custo_calça = valor_de_compra_calça + (valor_de_compra_calça * 0.20)
-        valor_com_lucro_calça = valor_de_custo_calça + (valor_de_custo_calça * 1.00)
-        print('O valor de venda deste produto é R$ {:.2f}'.format(valor_com_lucro_calça))
-    elif resposta == 3:
+        valor_de_compra_calça = float(input('Qual o valor de compra do produto: R$ ')) # Solicita o valor de compra da camiseta
+        valor_de_custo_calça = valor_de_compra_calça + (valor_de_compra_calça * 0.20)  # Calcula o custo com uma margem de 20%
+        valor_com_lucro_calça = valor_de_custo_calça + (valor_de_custo_calça * 1.00) # Calcula o lucro com uma margem de 100%
+        print('O valor de venda deste produto é R$ {:.2f}'.format(valor_com_lucro_calça)) # Exibe o valor de venda
+    elif resposta == 3: # Verifica a escolha do usuário
         print('---------' * 5)
         print('Você escolheu a opção Vestido.')
         print('---------' * 5)
-        valor_de_compra_vestido = float(input('Qual o valor de compra do produto: R$ '))
-        valor_de_custo_vestido = valor_de_compra_vestido + (valor_de_compra_vestido * 0.20)
-        valor_com_lucro_vestido = valor_de_custo_vestido + (valor_de_custo_vestido * 0.80)
-        print('O valor de venda deste produto é R$ {:.2f}'.format(valor_com_lucro_vestido))
-    elif resposta == 4:
+        valor_de_compra_vestido = float(input('Qual o valor de compra do produto: R$ ')) # Solicita o valor de compra da camiseta
+        valor_de_custo_vestido = valor_de_compra_vestido + (valor_de_compra_vestido * 0.20)  # Calcula o custo com uma margem de 20%
+        valor_com_lucro_vestido = valor_de_custo_vestido + (valor_de_custo_vestido * 0.80) # Calcula o lucro com uma margem de 80%
+        print('O valor de venda deste produto é R$ {:.2f}'.format(valor_com_lucro_vestido)) # Exibe o valor de venda
+    elif resposta == 4: # Verifica a escolha do usuário
         print('Encerrando a sua sessão')
         sleep(3)
         print('Sessão encerrada')
-        break
+        break # Encerra a sessão
     else:
         print('Opção inválida. Por favor, escolha um opção válida')
